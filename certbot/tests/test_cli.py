25ba66e4b1b4518d899cce156c24d60cf5be3cff
Jamie Hewland <jhewland@gmail.com>
2016-04-22 14:03:01 +0200

    Add basic CLI tests for --email required option

---
+++ certbot/tests/test_cli.py
@@ -0,0 +1,27 @@
+import re
+
+from click.testing import CliRunner
+
+from certbot.cli import main
+
+
+def test_email_required():
+    """
+    The program is expected to exit with an error code and message if '--email'
+    is not provided.
+    """
+    runner = CliRunner()
+    result = runner.invoke(main, [])
+
+    assert result.exit_code == 2
+    assert re.search('Error: Missing option "--email"', result.output)
+
+
+def test_email_provided():
+    """
+    The program is expected to exit with code 0 if '--email' is provided.
+    """
+    runner = CliRunner()
+    result = runner.invoke(main, ['--email', 'test@example.com'])
+
+    assert result.exit_code == 0
