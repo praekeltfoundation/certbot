c52787ca3574e06757a2d08c55482260ae4025fa
Jamie Hewland <jhewland@gmail.com>
2016-04-22 13:30:31 +0200

    Add default values to help message

--- certbot/cli.py
+++ certbot/cli.py
@@ -9,31 +9,37 @@ import sys
               required=True)
 @click.option('--server',
               help='The address for the ACME Directory Resource',
-              default='https://acme-v01.api.letsencrypt.org/directory')
+              default='https://acme-v01.api.letsencrypt.org/directory',
+              show_default=True)
 @click.option('--storage-dir',
               help='Path to directory for storing certificates')
 @click.option('--marathon', default='http://marathon.service.consul:8080',
-              help='The address for the Marathon HTTP API')
+              help='The address for the Marathon HTTP API',
+              show_default=True)
 @click.option('--listen',
               help='The address of the interface to bind to to receive '
                    'Marathon\'s event stream',
-              default='0.0.0.0')
-@click.option('--port', default='7000', type=int,
+              default='0.0.0.0',
+              show_default=True)
+@click.option('--port', default='7000', type=int, show_default=True,
               help='The port to bind to to receive Marathon\'s event stream')
 @click.option('--advertise', default='http://certbot.service.consul',
               help='The address to advertise to Marathon when registering for '
-                   'the event stream')
+                   'the event stream',
+              show_default=True)
 @click.option('--consul', default='http://consul.service.consul:8500',
-              help='The address for the Consul HTTP API')
+              help='The address for the Consul HTTP API',
+              show_default=True)
 @click.option('--consul-prefix', default='certbot',
               help='Prefix for all paths to certificates in Consul\'s '
-                   'key/value store')
+                   'key/value store',
+              show_default=True)
 @click.option('--poll',
               help='Periodically sync Marathon\'s state with Consul\'s every '
-                   '_n_ seconds',
+                   '_n_ seconds [default: disabled]',
               type=int)
 @click.option('--logfile',
-              help='Where to log output to (default stdout)',
+              help='Where to log output to [default: stdout]',
               type=click.File('a'),
               default=sys.stdout)
 @click.option('--debug',
