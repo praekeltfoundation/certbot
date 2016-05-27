2d9f0ce7d9694cf54e0f3f98cbfacae7aa8b9b0f
Jamie Hewland <jhewland@gmail.com>
2016-05-27 13:25:13 +0200

    Tweaks to our imaginary CLI

--- marathon_acme/cli.py
+++ marathon_acme/cli.py
@@ -8,8 +8,8 @@ import sys
               default='https://acme-v01.api.letsencrypt.org/directory',
               show_default=True)
 @click.option('--email',
-              help='Email address for Let\'s Encrypt certificate registration '
-                   'and recovery contact',
+              help=("Email address for Let's Encrypt certificate registration "
+                    "and recovery contact"),
               required=True)
 @click.option('--storage-dir',
               help='Path to directory for storing certificates')
@@ -17,15 +17,13 @@ import sys
               help='The address for the Marathon HTTP API',
               show_default=True)
 @click.option('--listen',
-              help='The address of the interface to bind to to receive '
-                   'Marathon\'s event stream',
-              default='0.0.0.0',
+              help=("The address of the interface and port to bind to to "
+                    "receive Marathon's event stream"),
+              default='0.0.0.0:7000',
               show_default=True)
-@click.option('--port', default='7000', type=int, show_default=True,
-              help='The port to bind to to receive Marathon\'s event stream')
 @click.option('--advertise', default='http://marathon-acme.marathon.mesos',
-              help='The address to advertise to Marathon when registering for '
-                   'the event stream',
+              help=('The address to advertise to Marathon when registering '
+                    'for the event stream'),
               show_default=True)
 @click.option('--poll',
               help=("Periodically check Marathon's state every _n_ seconds "
@@ -38,9 +36,9 @@ import sys
 @click.option('--debug',
               help='Log debug output',
               is_flag=True)
-def main(acme, email, storage_dir,                 # ACME
-         marathon, listen, port, advertise, poll,  # Marathon
-         logfile, debug):                          # Logging
+def main(acme, email, storage_dir,           # ACME
+         marathon, listen, advertise, poll,  # Marathon
+         logfile, debug):                    # Logging
     """
     A tool to automatically request, renew and distribute Let's Encrypt
     certificates for apps running on Seed Stack.
