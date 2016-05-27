a90dddb36087acba9fd99d24ec92ea729c7289d9
Jamie Hewland <jhewland@gmail.com>
2016-05-27 13:21:18 +0200

    Remove all references to Consul

--- marathon_acme/cli.py
+++ marathon_acme/cli.py
@@ -13,7 +13,7 @@ import sys
               required=True)
 @click.option('--storage-dir',
               help='Path to directory for storing certificates')
-@click.option('--marathon', default='http://marathon.service.consul:8080',
+@click.option('--marathon', default='http://marathon.mesos:8080',
               help='The address for the Marathon HTTP API',
               show_default=True)
 @click.option('--listen',
@@ -23,20 +23,13 @@ import sys
               show_default=True)
 @click.option('--port', default='7000', type=int, show_default=True,
               help='The port to bind to to receive Marathon\'s event stream')
-@click.option('--advertise', default='http://certbot.service.consul',
+@click.option('--advertise', default='http://marathon-acme.marathon.mesos',
               help='The address to advertise to Marathon when registering for '
                    'the event stream',
               show_default=True)
-@click.option('--consul', default='http://consul.service.consul:8500',
-              help='The address for the Consul HTTP API',
-              show_default=True)
-@click.option('--kv-prefix', default='certbot',
-              help='Prefix for all paths to certificates in Consul\'s '
-                   'key/value store',
-              show_default=True)
 @click.option('--poll',
-              help='Periodically sync Marathon\'s state with Consul\'s every '
-                   '_n_ seconds [default: disabled]',
+              help=("Periodically check Marathon's state every _n_ seconds "
+                    "[default: disabled]"),
               type=int)
 @click.option('--logfile',
               help='Where to log output to [default: stdout]',
@@ -45,10 +38,9 @@ import sys
 @click.option('--debug',
               help='Log debug output',
               is_flag=True)
-def main(acme, email, storage_dir,           # ACME
-         marathon, listen, port, advertise,  # Marathon
-         consul, kv_prefix, poll,            # Consul
-         logfile, debug):                    # Logging
+def main(acme, email, storage_dir,                 # ACME
+         marathon, listen, port, advertise, poll,  # Marathon
+         logfile, debug):                          # Logging
     """
     A tool to automatically request, renew and distribute Let's Encrypt
     certificates for apps running on Seed Stack.
