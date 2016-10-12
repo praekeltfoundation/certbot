6c763fbf3fa4fa844b6a06549b687ce0b4edd756
Jamie Hewland <jhewland@gmail.com>
2016-10-12 15:09:43 +0200

    Remove empty lines between imports

--- marathon_acme/tests/test_acme_util.py
+++ marathon_acme/tests/test_acme_util.py
@@ -1,15 +1,10 @@
 from acme.jose import JWKRSA
-
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
-
 from fixtures import TempDir
-
 from testtools import TestCase
 from testtools.matchers import Equals
-
 from twisted.python.filepath import FilePath
-
 from txacme.util import generate_private_key
 
 from marathon_acme.acme_util import maybe_key
