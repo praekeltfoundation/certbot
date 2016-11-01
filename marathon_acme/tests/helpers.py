bc0498421e4c5e282636ca79be4958b7b78e8e47
Jamie Hewland <jhewland@gmail.com>
2016-11-01 13:14:37 +0200

    Don't need fake_client anymore

--- marathon_acme/tests/helpers.py
+++ marathon_acme/tests/helpers.py
@@ -1,9 +1,6 @@
 import testtools
 from testtools.twistedsupport import AsynchronousDeferredRunTest
-from treq.client import HTTPClient
 from twisted.web.client import ProxyAgent, URI
-from twisted.web.server import Site
-from txfake import FakeServer
 
 
 class TestCase(testtools.TestCase):
@@ -22,13 +19,3 @@ class FakeServerAgent(ProxyAgent):
         return self._requestWithEndpoint(
             key, self._proxyEndpoint, method, parsedURI, headers,
             bodyProducer, parsedURI.originForm)
-
-
-def fake_client(resource):
-    """
-    Build a Treq HTTPClient aroudn a FakeServer to make requests against the
-    given resource.
-    """
-    fake_server = FakeServer(Site(resource))
-    fake_agent = FakeServerAgent(fake_server.endpoint)
-    return HTTPClient(fake_agent)
