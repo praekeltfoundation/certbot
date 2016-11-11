4ae861e16ad41c0a55557468af19ce047a8826a9
Jamie Hewland <jhewland@gmail.com>
2016-11-11 12:53:13 +0200

    cli: Move parser out of main()

--- marathon_acme/cli.py
+++ marathon_acme/cli.py
@@ -14,42 +14,43 @@ from marathon_acme.clients import MarathonClient, MarathonLbClient
 from marathon_acme.service import MarathonAcme
 
 
+parser = argparse.ArgumentParser(
+    description='Automatically manage ACME certificates for Marathon apps')
+parser.add_argument('-a', '--acme',
+                    help='The address for the ACME Directory Resource '
+                         '(default: %(default)s)',
+                    default=(
+                        'https://acme-v01.api.letsencrypt.org/directory'))
+parser.add_argument('-m', '--marathon',
+                    help='The address for the Marathon HTTP API (default: '
+                         '%(default)s)',
+                    default='http://marathon.mesos:8080')
+parser.add_argument('-l', '--lb', nargs='+',
+                    help='The address for the marathon-lb HTTP API '
+                         '(default: %(default)s)',
+                    default='http://marathon-lb.marathon.mesos:9090')
+parser.add_argument('-g', '--group',
+                    help='The marathon-lb group to issue certificates for '
+                         '(default: %(default)s)',
+                    default='external')
+parser.add_argument('--listen',
+                    help='The address for the port to listen on (default: '
+                         '%(default)s)',
+                    default='0.0.0.0:8000')
+parser.add_argument('--log-level',
+                    help='The minimum severity level to log messages at '
+                         '(default: %(default)s)',
+                    choices=['debug', 'info', 'warn', 'error', 'critical'],
+                    default='info'),
+parser.add_argument('storage_dir', metavar='storage-dir',
+                    help='Path to directory for storing certificates')
+
+
 def main(reactor, raw_args=sys.argv[1:]):
     """
     A tool to automatically request, renew and distribute Let's Encrypt
     certificates for apps running on Marathon and served by marathon-lb.
     """
-    parser = argparse.ArgumentParser(
-        description='Automatically manage ACME certificates for Marathon apps')
-    parser.add_argument('-a', '--acme',
-                        help='The address for the ACME Directory Resource '
-                             '(default: %(default)s)',
-                        default=(
-                            'https://acme-v01.api.letsencrypt.org/directory'))
-    parser.add_argument('-m', '--marathon',
-                        help='The address for the Marathon HTTP API (default: '
-                             '%(default)s)',
-                        default='http://marathon.mesos:8080')
-    parser.add_argument('-l', '--lb', nargs='+',
-                        help='The address for the marathon-lb HTTP API '
-                             '(default: %(default)s)',
-                        default='http://marathon-lb.marathon.mesos:9090')
-    parser.add_argument('-g', '--group',
-                        help='The marathon-lb group to issue certificates for '
-                             '(default: %(default)s)',
-                        default='external')
-    parser.add_argument('--listen',
-                        help='The address for the port to listen on (default: '
-                             '%(default)s)',
-                        default='0.0.0.0:8000')
-    parser.add_argument('--log-level',
-                        help='The minimum severity level to log messages at '
-                             '(default: %(default)s)',
-                        choices=['debug', 'info', 'warn', 'error', 'critical'],
-                        default='info'),
-    parser.add_argument('storage_dir', metavar='storage-dir',
-                        help='Path to directory for storing certificates')
-
     args = parser.parse_args(raw_args)
 
     # Set up logging
