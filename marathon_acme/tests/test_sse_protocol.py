043aee19da9b7357f790f49305d83a47ae259c98
Jamie Hewland <jhewland@gmail.com>
2017-11-27 13:26:34 +0200

    SseProtocol: Add initial timeout test
    * Switch to pytest fixtures

--- marathon_acme/tests/test_sse_protocol.py
+++ marathon_acme/tests/test_sse_protocol.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 from testtools.assertions import assert_that
 from testtools.matchers import Equals, Is
 from testtools.twistedsupport import succeeded
+
+from twisted.internet.task import Clock
 
 from marathon_acme.sse_protocol import SseProtocol
 
@@ -13,254 +17,286 @@ class DummyTransport(object):
         self.disconnecting = True
 
 
+@pytest.fixture
+def messages():
+    return list()
+
+
+@pytest.fixture
+def protocol(messages):
+    return make_protocol(messages)
+
+
+def make_protocol(messages=None, **kwargs):
+    if messages is None:
+        messages = list()
+
+    def handler(event, data):
+        messages.append((event, data))
+
+    protocol = SseProtocol(handler, **kwargs)
+    protocol.transport = DummyTransport()
+    return protocol
+
+
 class TestSseProtocol(object):
-    def setup_method(self):
-        self.messages = []
-
-        def append_message(event, data):
-            self.messages.append((event, data))
-        self.protocol = SseProtocol(append_message)
-
-        self.transport = DummyTransport()
-        self.protocol.transport = self.transport
-
-    def test_default_event(self):
+    def test_default_event(self, protocol, messages):
         """
         When data is received, followed by a blank line, the default event
         type, 'message', should be used.
         """
-        self.protocol.dataReceived(b'data:hello\r\n\r\n')
+        protocol.dataReceived(b'data:hello\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello')]))
+        assert_that(messages, Equals([('message', 'hello')]))
 
-    def test_multiline_data(self):
+    def test_multiline_data(self, protocol, messages):
         """
         When multiple lines of data are specified in a single event, those
         lines should be received by the handler with a '\n' character
         separating them.
         """
-        self.protocol.dataReceived(b'data:hello\r\ndata:world\r\n\r\n')
+        protocol.dataReceived(b'data:hello\r\ndata:world\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello\nworld')]))
+        assert_that(messages, Equals([('message', 'hello\nworld')]))
 
-    def test_different_newlines(self):
+    def test_different_newlines(self, protocol, messages):
         """
         When data is received with '\r\n', '\n', or '\r', lines should be split
         on those characters.
         """
-        self.protocol.dataReceived(b'data:hello\ndata:world\r\r\n')
+        protocol.dataReceived(b'data:hello\ndata:world\r\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello\nworld')]))
+        assert_that(messages, Equals([('message', 'hello\nworld')]))
 
-    def test_empty_data(self):
+    def test_empty_data(self, protocol, messages):
         """
         When the data field is specified in an event but no data is given, the
         handler should receive a message with empty data.
         """
-        self.protocol.dataReceived(b'data:\r\n\r\n')
+        protocol.dataReceived(b'data:\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', '')]))
+        assert_that(messages, Equals([('message', '')]))
 
-    def test_no_data(self):
+    def test_no_data(self, protocol, messages):
         """
         When the data field is not specified and the event is completed, the
         handler should not be called.
         """
-        self.protocol.dataReceived(b'\r\n')
+        protocol.dataReceived(b'\r\n')
 
-        assert_that(self.messages, Equals([]))
+        assert_that(messages, Equals([]))
 
-    def test_space_before_value(self):
+    def test_space_before_value(self, protocol, messages):
         """
         When a field/value pair is received, and there is a space before the
         value, the leading space should be stripped.
         """
-        self.protocol.dataReceived(b'data: hello\r\n\r\n')
+        protocol.dataReceived(b'data: hello\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello')]))
+        assert_that(messages, Equals([('message', 'hello')]))
 
-    def test_space_before_value_strip_only_first_space(self):
+    def test_space_before_value_strip_only_first_space(
+            self, protocol, messages):
         """
         When a field/value pair is received, and there are multiple spaces at
         the start of the value, the leading space should be stripped and the
         other spaces left intact.
         """
-        self.protocol.dataReceived(
+        protocol.dataReceived(
             'data:{}\r\n\r\n'.format(' ' * 4).encode('utf-8'))
 
-        assert_that(self.messages, Equals([('message', ' ' * 3)]))
+        assert_that(messages, Equals([('message', ' ' * 3)]))
 
-    def test_custom_event(self):
+    def test_custom_event(self, protocol, messages):
         """
         If a custom event is set for an event, a the handler should be called
         with the correct event.
         """
-        self.protocol.dataReceived(b'event:my_event\r\n')
-        self.protocol.dataReceived(b'data:hello\r\n\r\n')
+        protocol.dataReceived(b'event:my_event\r\n')
+        protocol.dataReceived(b'data:hello\r\n\r\n')
 
-        assert_that(self.messages, Equals([('my_event', 'hello')]))
+        assert_that(messages, Equals([('my_event', 'hello')]))
 
-    def test_multiple_events(self):
+    def test_multiple_events(self, protocol, messages):
         """
         If multiple different event types are received, the handler should
         receive the different event types and their corresponding data.
         """
-        self.protocol.dataReceived(b'event:test1\r\n')
-        self.protocol.dataReceived(b'data:hello\r\n\r\n')
-        self.protocol.dataReceived(b'event:test2\r\n')
-        self.protocol.dataReceived(b'data:world\r\n\r\n')
+        protocol.dataReceived(b'event:test1\r\n')
+        protocol.dataReceived(b'data:hello\r\n\r\n')
+        protocol.dataReceived(b'event:test2\r\n')
+        protocol.dataReceived(b'data:world\r\n\r\n')
 
-        assert_that(self.messages, Equals([
+        assert_that(messages, Equals([
             ('test1', 'hello'),
             ('test2', 'world')
         ]))
 
-    def test_id_ignored(self):
+    def test_id_ignored(self, protocol, messages):
         """
         When the id field is included in an event, it should be ignored.
         """
-        self.protocol.dataReceived(b'data:hello\r\n')
-        self.protocol.dataReceived(b'id:123\r\n\r\n')
+        protocol.dataReceived(b'data:hello\r\n')
+        protocol.dataReceived(b'id:123\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello')]))
+        assert_that(messages, Equals([('message', 'hello')]))
 
-    def test_retry_ignored(self):
+    def test_retry_ignored(self, protocol, messages):
         """
         When the retry field is included in an event, it should be ignored.
         """
-        self.protocol.dataReceived(b'data:hello\r\n')
-        self.protocol.dataReceived(b'retry:123\r\n\r\n')
+        protocol.dataReceived(b'data:hello\r\n')
+        protocol.dataReceived(b'retry:123\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello')]))
+        assert_that(messages, Equals([('message', 'hello')]))
 
-    def test_unknown_field_ignored(self):
+    def test_unknown_field_ignored(self, protocol, messages):
         """
         When an unknown field is included in an event, it should be ignored.
         """
-        self.protocol.dataReceived(b'data:hello\r\n')
-        self.protocol.dataReceived(b'somefield:123\r\n\r\n')
+        protocol.dataReceived(b'data:hello\r\n')
+        protocol.dataReceived(b'somefield:123\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello')]))
+        assert_that(messages, Equals([('message', 'hello')]))
 
-    def test_leading_colon_ignored(self):
+    def test_leading_colon_ignored(self, protocol, messages):
         """
         When a line is received starting with a ':' character, the line should
         be ignored.
         """
-        self.protocol.dataReceived(b'data:hello\r\n')
-        self.protocol.dataReceived(b':123abc\r\n\r\n')
+        protocol.dataReceived(b'data:hello\r\n')
+        protocol.dataReceived(b':123abc\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello')]))
+        assert_that(messages, Equals([('message', 'hello')]))
 
-    def test_missing_colon(self):
+    def test_missing_colon(self, protocol, messages):
         """
         When a line is received that doesn't contain a ':' character, the whole
         line should be treated as the field and the value should be an empty
         string.
         """
-        self.protocol.dataReceived(b'data\r\n')
-        self.protocol.dataReceived(b'data:hello\r\n\r\n')
+        protocol.dataReceived(b'data\r\n')
+        protocol.dataReceived(b'data:hello\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', '\nhello')]))
+        assert_that(messages, Equals([('message', '\nhello')]))
 
-    def test_trim_only_last_newline(self):
+    def test_trim_only_last_newline(self, protocol, messages):
         """
         When multiline data is received, only the final newline character
         should be stripped before the data is passed to the handler.
         """
-        self.protocol.dataReceived(b'data:\r')
-        self.protocol.dataReceived(b'data:\n')
-        self.protocol.dataReceived(b'data:\r\n\r\n')
+        protocol.dataReceived(b'data:\r')
+        protocol.dataReceived(b'data:\n')
+        protocol.dataReceived(b'data:\r\n\r\n')
 
-        assert_that(self.messages, Equals([('message', '\n\n')]))
+        assert_that(messages, Equals([('message', '\n\n')]))
 
-    def test_multiple_data_parts(self):
+    def test_multiple_data_parts(self, protocol, messages):
         """
         When data is received in multiple parts, the parts should be collected
         to form the lines of the event.
         """
-        self.protocol.dataReceived(b'data:')
-        self.protocol.dataReceived(b' hello\r\n')
-        self.protocol.dataReceived(b'\r\n')
+        protocol.dataReceived(b'data:')
+        protocol.dataReceived(b' hello\r\n')
+        protocol.dataReceived(b'\r\n')
 
-        assert_that(self.messages, Equals([('message', 'hello')]))
+        assert_that(messages, Equals([('message', 'hello')]))
 
-    def test_unicode_data(self):
+    def test_unicode_data(self, protocol, messages):
         """
         When unicode data encoded as UTF-8 is received, the characters should
         be decoded correctly.
         """
-        self.protocol.dataReceived(u'data:hëlló\r\n\r\n'.encode('utf-8'))
+        protocol.dataReceived(u'data:hëlló\r\n\r\n'.encode('utf-8'))
 
-        assert_that(self.messages, Equals([('message', u'hëlló')]))
+        assert_that(messages, Equals([('message', u'hëlló')]))
 
-    def test_line_too_long(self):
+    def test_line_too_long(self, protocol):
         """
         When a line is received that is beyond the maximum allowed length,
         the transport should be in 'disconnecting' state due to a request to
         lose the connection.
         """
-        self.protocol.MAX_LENGTH = 8  # Very long bytearrays slow down tests
-        self.protocol.dataReceived('data:{}\r\n\r\n'.format(
-            'x' * (self.protocol.MAX_LENGTH + 1)).encode('utf-8'))
+        protocol.MAX_LENGTH = 8  # Very long bytearrays slow down tests
+        protocol.dataReceived('data:{}\r\n\r\n'.format(
+            'x' * (protocol.MAX_LENGTH + 1)).encode('utf-8'))
 
-        assert_that(self.transport.disconnecting, Equals(True))
+        assert_that(protocol.transport.disconnecting, Equals(True))
 
-    def test_incomplete_line_too_long(self):
+    def test_incomplete_line_too_long(self, protocol):
         """
         When a part of a line is received that is beyond the maximum allowed
         length, the transport should be in 'disconnecting' state due to a
         request to lose the connection.
         """
-        self.protocol.MAX_LENGTH = 8  # Very long bytearrays slow down tests
-        self.protocol.dataReceived('data:{}'.format(
-            'x' * (self.protocol.MAX_LENGTH + 1)).encode('utf-8'))
+        protocol.MAX_LENGTH = 8  # Very long bytearrays slow down tests
+        protocol.dataReceived('data:{}'.format(
+            'x' * (protocol.MAX_LENGTH + 1)).encode('utf-8'))
 
-        assert_that(self.transport.disconnecting, Equals(True))
+        assert_that(protocol.transport.disconnecting, Equals(True))
 
-    def test_transport_disconnecting(self):
+    def test_transport_disconnecting(self, protocol, messages):
         """
         When the transport for the protocol is disconnecting, processing should
         be halted.
         """
-        self.transport.disconnecting = True
-        self.protocol.dataReceived(b'data:hello\r\n\r\n')
+        protocol.transport.disconnecting = True
+        protocol.dataReceived(b'data:hello\r\n\r\n')
 
-        assert_that(self.messages, Equals([]))
+        assert_that(messages, Equals([]))
 
-    def test_transport_connection_lost(self):
+    def test_transport_connection_lost(self, protocol):
         """
         When the connection is lost, the finished deferred should be called.
         """
-        finished = self.protocol.when_finished()
+        finished = protocol.when_finished()
 
-        self.protocol.connectionLost()
+        protocol.connectionLost()
 
         assert_that(finished, succeeded(Is(None)))
 
-    def test_transport_connection_lost_no_callback(self):
+    def test_transport_connection_lost_no_callback(self, protocol):
         """
         When the connection is lost and the finished deferred hasn't been set,
         nothing should happen.
         """
-        self.protocol.connectionLost()
+        protocol.connectionLost()
 
-    def test_multiple_events_resets_the_event_type(self):
+    def test_multiple_events_resets_the_event_type(self, protocol, messages):
         """
         After an event is consumed with a custom event type, the event type
         should be reset to the default, and the handler should receive further
         messages with the default event type.
         """
         # Event 1
-        self.protocol.dataReceived(b'event:status\r\n')
-        self.protocol.dataReceived(b'data:hello\r\n')
-        self.protocol.dataReceived(b'\r\n')
+        protocol.dataReceived(b'event:status\r\n')
+        protocol.dataReceived(b'data:hello\r\n')
+        protocol.dataReceived(b'\r\n')
 
         # Event 2
-        self.protocol.dataReceived(b'data:world\r\n')
-        self.protocol.dataReceived(b'\r\n')
+        protocol.dataReceived(b'data:world\r\n')
+        protocol.dataReceived(b'\r\n')
 
-        assert_that(self.messages, Equals([
+        assert_that(messages, Equals([
             ('status', 'hello'),
             ('message', 'world')
         ]))
+
+    def test_timeout(self):
+        """
+        When a timeout value is set, the timeout should be triggered once the
+        timeout has elapsed and should disconnect the transport.
+        """
+        timeout = 5
+        clock = Clock()
+        protocol = make_protocol(timeout=timeout, reactor=clock)
+        protocol.connectionMade()
+
+        [delayed_call] = clock.getDelayedCalls()
+        assert_that(delayed_call.getTime(), Equals(clock.seconds() + timeout))
+        assert_that(protocol.transport.disconnecting, Is(False))
+
+        clock.advance(timeout)
+        # Timeout should be triggered
+        assert_that(clock.getDelayedCalls(), Equals([]))
+        assert_that(protocol.transport.disconnecting, Is(True))
