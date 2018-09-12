229b74fe585f42c8b9e71c56e4e0547e610de365
Jamie Hewland <jhewland@gmail.com>
2018-09-12 13:21:40 +0200

    VaultClient: Add doc note about CasError

--- marathon_acme/clients/vault.py
+++ marathon_acme/clients/vault.py
@@ -113,6 +113,10 @@ class VaultClient(HTTPClient):
     def create_or_update_kv2(self, path, data, cas=None, mount_path='secret'):
         """
         Create or update some data in a key/value version 2 secret engine.
+
+        :raises CasError:
+            Raises an error if the ``cas`` value, when provided, doesn't match
+            Vault's version for the key.
         """
         params = {
             'options': {},
