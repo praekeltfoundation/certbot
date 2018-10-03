811ec0dc16cc961e4fd5cf48925f971f3692927b
Jamie Hewland <jhewland@gmail.com>
2018-10-03 13:49:51 +0200

    Add a function to store the LE client key in Vault

--- marathon_acme/acme_util.py
+++ marathon_acme/acme_util.py
@@ -21,6 +21,19 @@ from txacme.util import generate_private_key
 from zope.interface import implementer
 
 
+def _load_pem_private_key_bytes(key_bytes):
+    return serialization.load_pem_private_key(
+        key_bytes, password=None, backend=default_backend())
+
+
+def _dump_pem_private_key_bytes(key):
+    return key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+
 def maybe_key(pem_path):
     """
     Set up a client key if one does not exist already.
@@ -33,21 +46,40 @@ def maybe_key(pem_path):
     """
     acme_key_file = pem_path.child(u'client.key')
     if acme_key_file.exists():
-        key = serialization.load_pem_private_key(
-            acme_key_file.getContent(),
-            password=None,
-            backend=default_backend()
-        )
+        key = _load_pem_private_key_bytes(acme_key_file.getContent())
     else:
         key = generate_private_key(u'rsa')
-        acme_key_file.setContent(
-            key.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.TraditionalOpenSSL,
-                encryption_algorithm=serialization.NoEncryption()
-            )
-        )
+        acme_key_file.setContent(_dump_pem_private_key_bytes(key))
     return JWKRSA(key=key)
+
+
+def maybe_key_vault(client, mount_path):
+    """
+    Set up a client key in Vault if one does not exist already.
+
+    :param client:
+        The Vault API client to use.
+    :param mount_path:
+        The Vault key/value mount path to use.
+    """
+    d = client.read_kv2('client_key', mount_path=mount_path)
+
+    def get_or_create_key(client_key):
+        if client_key is not None:
+            key_data = client_key['data']['data']
+            key = _load_pem_private_key_bytes(key_data['key'].encode('utf-8'))
+            return JWKRSA(key=key)
+        else:
+            key = generate_private_key(u'rsa')
+            key_data = {
+                'key': _dump_pem_private_key_bytes(key).decode('utf-8')
+            }
+            d = client.create_or_update_kv2(
+                'client_key', key_data, mount_path=mount_path)
+
+            return d.addCallback(lambda _result: JWKRSA(key=key))
+
+    return d.addCallback(get_or_create_key)
 
 
 def create_txacme_client_creator(reactor, url, key, alg=RS256):
