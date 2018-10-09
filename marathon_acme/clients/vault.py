86c027fb20379f119921c12aca50e6ff1779ca20
Jamie Hewland <jhewland@gmail.com>
2018-10-09 13:19:29 +0200

    Update docs to list which VAULT_ env vars are/are not supported

--- marathon_acme/clients/vault.py
+++ marathon_acme/clients/vault.py
@@ -46,13 +46,30 @@ class VaultClient(HTTPClient):
         """
         Create a Vault client with configuration from the environment. Supports
         a limited number of the available config options:
-        https://github.com/hashicorp/vault/blob/v0.11.2/api/client.go#L28-L40
+        https://www.vaultproject.io/docs/commands/index.html#environment-variables
+        https://github.com/hashicorp/vault/blob/v0.11.3/api/client.go#L28-L40
+
+        Supported:
+        - ``VAULT_ADDR``
+        - ``VAULT_CACERT``
+        - ``VAULT_CLIENT_CERT``
+        - ``VAULT_CLIENT_KEY``
+        - ``VAULT_TLS_SERVER_NAME``
+        - ``VAULT_TOKEN``
+
+        Not currently supported:
+        - ``VAULT_CAPATH``
+        - ``VAULT_CLIENT_TIMEOUT``
+        - ``VAULT_MAX_RETRIES``
+        - ``VAULT_MFA``
+        - ``VAULT_RATE_LIMIT``
+        - ``VAULT_SKIP_VERIFY``
+        - ``VAULT_WRAP_TTL``
         """
         address = env.get('VAULT_ADDR', 'https://127.0.0.1:8200')
         # This seems to be what the Vault CLI defaults to
         token = env.get('VAULT_TOKEN', 'TEST')
 
-        # TODO: insecure = _get_environ_bool(env, 'VAULT_SKIP_VERIFY')
         ca_cert = env.get('VAULT_CACERT')
         tls_server_name = env.get('VAULT_TLS_SERVER_NAME')
         client_cert = env.get('VAULT_CLIENT_CERT')
